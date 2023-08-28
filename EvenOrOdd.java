818261df993029b2d5740b75100ebc20c5e2953e
Francis Patawah <phrancisp0@gmail.com>
2023-08-27 22:29:03 -0400

    8kyu even or odd

---
+++ EvenOrOdd.java
@@ -0,0 +1,14 @@
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+    /*
+    If the modulo is zero, you have an even number.
+    */
+      return number % 2 == 0 ? "Even" : "Odd";        
+    }
+}
+
+/*
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+*/
